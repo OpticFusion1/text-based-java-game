3b726bae5b0f235aa7fe3d81aa50a96e192cf5e0
Michael Murphy <murphym1219@gmail.com>
2018-06-22 01:01:12 -0400

    Update Adult.java

--- Adult.java
+++ Adult.java
@@ -6,9 +6,5 @@ class Adult extends NPC {
 	}
 	
 	public void look(){
-		//look doesn't work yet
-		//make it work
-		//call it every time an action is made by player
-		//THINK FUNCTIONAL PROGRAMMING! There's no reason to constantly be checking for inputs 
 	}
 }
